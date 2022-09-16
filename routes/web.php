85d3782d6ef27a1a568558e85e2949493260840f
pedrow04 <pedrocamacho701@gmail.com>
2022-09-15 23:35:53 -0300

    atualizando repositorio

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
-// Route::view('/Pedro','jogos');
-
-
-
-//Route::view('/jogos', 'jogos',['name'=> 'GTA']);
-
-//Route::get('/jogos/{name}', function($name){
-//    return view('jogos',['nomeJogo'=>$name]);
-//});
-
-Route::get('/jogos',[JogosController::class,'index'] );
-
-Route::get('/home', function () {
-    return view('welcome');
-})->name('home-index');
+Route::prefix('jogos')->group(function(){
+    Route::get('/',[JogosController::class,'index'])->name('jogos-index');
+    Route::get('/create',[JogosController::class,'create'])->name('jogos-create');
+    Route::post('/',[JogosController::class,'store'])->name('jogos-store');
+    Route::get('/{id}/edit',[JogosController::class,'edit'])->where('id', '[0-9]+')->name('jogos-edit');
+    Route::put('/{id}',[JogosController::class,'update'])->where('id', '[0-9]+')->name('jogos-update');
+    Route::delete('/{id}',[JogosController::class,'destroy'])->where('id', '[0-9]+')->name('jogos-destroy');
+});
 
 Route::fallback(function(){
     return"Erro !";
